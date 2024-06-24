380b6694417863fdc734555f1b8acab787666950
yohanlimapo <yohan.limapo@gmail.com>
2024-06-24 15:01:25 -0300

    fim-projeto

--- java-basico/src/ContaTerminal.java
+++ java-basico/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o número da conta: ");
+        int numero = sc.nextInt();
+        System.out.println("Digite a agência da conta: ");
+        String agencia = sc.next();
+        System.out.println("Digite o nome da conta: ");
+        String nomeCliente = sc.next();
+        System.out.println("Digite o saldo da conta: ");
+        Float saldo = sc.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", " + "conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
